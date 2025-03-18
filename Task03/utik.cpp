1b8ff7a205f666a6bd8630ecabc2a1ee2eb0495a
Danila Sosnov <dsosnov2009@gmail.com>
2025-03-18 18:44:58 +0300

    Upd

--- Task03/utik.cpp
+++ Task03/utik.cpp
@@ -13,3 +13,15 @@ string convert(int** matrix, int n, int m) {
 
 	return s;
 }
+
+void init(int** matrix, int n, int m, int b, int a)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			matrix[n][m] = rand() % (b - a + 1) + a;
+		}
+
+	}
+}
